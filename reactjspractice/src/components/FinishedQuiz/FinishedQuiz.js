8624e4b78b6868cc811a274d0ab10e87782bc273
logshoy <logshoy@gmail.com>
2020-05-29 19:50:51 +0500

    lesson 73

--- reactjspractice/src/components/FinishedQuiz/FinishedQuiz.js
+++ reactjspractice/src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './FinishedQuiz.module.css'
 import Button from '../UI/Button/Button'
+import {Link} from 'react-router-dom'
 
 const FinishedQuiz = props => {
     const succescCount = Object.keys(props.results).reduce((total, key) => {
@@ -37,7 +38,9 @@ const FinishedQuiz = props => {
 
             <div>
                 <Button onClick={props.onRetry} type="Primary">Повторить</Button>
-                <Button type="Success">Перейти в список тестов</Button>
+                <Link to="/">
+                 <Button type="Success">Перейти в список тестов</Button>
+                </Link>
             </div>
         </div>
     )
---
+++ reactjspractice/src/containers/Auth/Auth.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+
+export default class Auth extends React.Component {
+    
+    loginHandler = () => {
+        
+    }
+
+    registerHandler = () => {
+
+    }
+    
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form
+                        onSubmit={this.submitHandler}
+                        className={classes.AuthForm}>
+                         <input type="text" />
+                         <input type="number" />
+
+                        <Button type="Success" 
+                            onClick={this.loginHandler}
+                        >Войти
+                        </Button>
+                        <Button type="Primary" 
+                            onClick={this.registerHandler}
+                        >Зарегистрироваться
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
