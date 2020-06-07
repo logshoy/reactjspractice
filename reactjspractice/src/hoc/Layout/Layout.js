4bd52e4d5d1bca1c1e966a08513bf73bf2c1b6f7
logshoy <logshoy@gmail.com>
2020-06-07 14:11:19 +0500

    lesson 108 done

--- reactjspractice/src/hoc/Layout/Layout.js
+++ reactjspractice/src/hoc/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Layout.module.css'
 import MenuToggle from '../../components/Navigation/MenuToogle/MenuToogle'
 import Drawer from '../../components/Navigation/Drawer/Drawer'
+import { connect } from 'react-redux';
 
 class Layout extends React.Component {
 
@@ -26,8 +27,10 @@ class Layout extends React.Component {
         return(
             <div className={classes.Layout}>  
 
-            <Drawer isOpen={this.state.menu} 
+            <Drawer 
+                isOpen={this.state.menu} 
                 onClose={this.menuCloseHandler}
+                isAuthenticated={this.props.isAuthenticated}
             />
                 
             <MenuToggle 
@@ -43,4 +46,10 @@ class Layout extends React.Component {
     }
 }
 
-export default Layout
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.auth.token
+    }
+}
+
+export default connect(mapStateToProps)(Layout)
